b0e5e16ce247b7b71cbd95e116a2a6930d4d458d
pavel_batotsyrenov <webmoney91rus@yandex.ru>
2021-03-22 01:49:08 +0300

    first atempt

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,30 +1,79 @@
-﻿// Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
+#include <omp.h>
+#include <string>
+
 using namespace std;
 
 void readMatrixFromFile(string fileName, float**& matrix);
 void printMatrix(float**& matrix, const char* msg);
 float findDeterminant(float**& matrix);
+void findDeterminantAndWriteResults(float**& matrix, float& avgTime);
 void gauss(float**& matrix);
 
 int matrixSize;
-
+int a;
+int numThreads = 0;
+int dynamic = 2;
 
 int main(int argc, const char **argv)
 {
+    if (argc != 3) {
+        cerr << "Incorrect program arguments used. There should be 2 argumets: input_file_name and threads number [-1;" << omp_get_max_threads() << "]";
+        exit(1);
+    }
+
     float** matrix;
+    a = stoi(argv[2]);
+
+    if (a < -1) {
+        cerr << "Incorrect threads number. There is allowed only values from -1 until " << omp_get_max_threads();
+        exit(1);
+    } else if (a > omp_get_max_threads()) {
+        cerr << "Incorrect threads number. You can use only " << omp_get_max_threads() << " threads";
+        exit(1);
+    }
+    //a = 0;
+    if (a == -1) {
+        numThreads = 1;
+    } else {
+        numThreads = a == 0 ? omp_get_max_threads() : a;
+    }
 
     readMatrixFromFile(argv[1], matrix);
-    printMatrix(matrix, (char*) "matrix from file");
-    float result = findDeterminant(matrix);
-    printMatrix(matrix, (char*)"matrix after Gauss");
 
-    cout << "\nDeterminant is " << fixed << result << "\n\n";
+    clock_t start = clock();
+    float result = findDeterminant(matrix);
+    float execTime = clock() - start;
+
+    printf("Determinant: %g\n", result);
+    printf("\nTime (%i thread(s)) %f ms\n", a == -1 ? a : numThreads, execTime);
+    
+    for (int i = 0; i < matrixSize; i++) 
+        delete[] matrix[i];
+    delete[] matrix;
+    exit(0);
 }
 
+void testDynamicParam(float**& matrix) {
+    for (int k = 2; k < 9; k++) {
+        numThreads = k;
+        for (int j = 1; j < 11; j++) {
+            dynamic = j;
+            float avgTime = 0;
+            for (int i = 0; i < 100; i++)
+                findDeterminantAndWriteResults(matrix, avgTime);
+            cout << "\n\ndynamic param is: " << j << " num threads: " << k << " Time:  " << avgTime / 100 << "\n\n";
+        }
+    }
+}
+
+void findDeterminantAndWriteResults(float**& matrix, float& avgTime) {
+    clock_t start = clock();
+    float result = findDeterminant(matrix);
+    float execTime = clock() - start;
+    avgTime += execTime;
+}
 
 void readMatrixFromFile(string fileName, float**& matrix) {
     ifstream file(fileName);
@@ -34,7 +83,6 @@ void readMatrixFromFile(string fileName, float**& matrix) {
     }
     
     file >> matrixSize;
-    cout << matrixSize;
 
     matrix = new float* [matrixSize];
     for (int i = 0; i < matrixSize; i++) {
@@ -49,46 +97,57 @@ float findDeterminant(float**& matrix) {
     
     gauss(matrix);
 
-    float determinant = 1;
-    for (int i = 0; i < matrixSize; i++) {
-        determinant *= matrix[i][i];
+    float* resultsArr = new float[numThreads];
+    for (int i = 0; i < numThreads; i++) {
+        resultsArr[i] = 1;
     }
-    
+
+#pragma omp parallel if (a > -1) num_threads(numThreads)
+    {
+        float localResult = 1;
+
+#pragma omp for schedule(dynamic)
+        for (int i = 0; i < matrixSize; i++) {
+            localResult *= matrix[i][i];
+        }
+        resultsArr[omp_get_thread_num()] = localResult;
+    }
+    float determinant = 1;
+
+    for (int i = 0; i < numThreads; i++) {
+        determinant *= resultsArr[i];
+    }
+
+    delete[] resultsArr;
     return determinant;
 }
 
 void gauss(float**& matrix) {
-    float temp;
-
-    for (int i = 0; i < matrixSize - 1; i++) {
+    
+    for (int i = 0; i < matrixSize - 1; i++) {        
+#pragma omp parallel for if(a > -1) num_threads(numThreads) schedule(dynamic, dynamic)
         for (int j = i + 1; j < matrixSize; j++) {
+            float temp;
             temp = matrix[i][i] == 0 ? 0 : -matrix[j][i] / matrix[i][i];
             for (int t = 0; t < matrixSize; t++) {
                 matrix[j][t] += matrix[i][t] * temp;
             }
         }
     }
+        
+
 }
 
 void printMatrix(float**& matrix, const char* msg) {
     
-    cout << "\n" << msg << "\nMatrix:\n\n";
-    
+    cout << "\n" << msg << "\nMatrix:\n\n"; 
     for (int i = 0; i < matrixSize; i++) {
         for (int j = 0; j < matrixSize; j++) {
+            cout.width(8);
+            cout.precision(2);
             cout << fixed << matrix[i][j] << " ";
         }
         cout << "\n";
     }
     cout << "\n";
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
